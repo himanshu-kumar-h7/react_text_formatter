e651d8b67edf7fc2a59fed03039c763edd493b8b
Himanshu <himanshu@gmail.com>
2024-05-04 14:38:22 +0530

    second-commit

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -14,7 +14,7 @@ export default function Form({example,setText,handleAlert}) {
     handleAlert("Upper case done",'Well done')
    }
    const wordCounter=()=>{
-    
+    console.log("aaaa")
    }
 
    const onClicklow=()=>{
