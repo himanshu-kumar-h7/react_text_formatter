e0048200dc1e55e2aa8d22dbcab2f7f1a57bc3de
Himanshu <himanshu@gmail.com>
2024-05-06 18:09:14 +0530

    Change in Alert to fix layout

--- src/App.js
+++ src/App.js
@@ -5,48 +5,48 @@ import { useState } from "react";
 import AboutUs from "./AboutUs";
 import Alert from "./Components/Alert";
 
+
+
+
 function App() {
   const [text, setText] = useState("");
   const [mode, setMode] = useState("light");
   const [alert, setAlert] = useState(null);
   const handleAlert = (message, type) => {
     setAlert({
-      message : message,
-       type : type
-       });
-       setTimeout(() => {
-        setAlert(null)
-       }, 2000);
+      message: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
   };
   const handleMode1 = () => {
     if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "#6CB4EE";
-      handleAlert('Dark mode is enabled', 'Successs')
-     //display
+      handleAlert("Dark mode is enabled", "Successs");
+      //display
       // setInterval(() => {
       //   document.title = 'Textformatter - Home - Dark mode';
       // }, 1000);
       // setInterval(() => {
       //   document.title = 'Textformatter - 2 ';
       // }, 2000);
-      document.title = 'Textformatter - Home dark enabled ';
+      document.title = "Textformatter - Home dark enabled ";
       setTimeout(() => {
-        
-        document.title = 'Textformatter - Home ';
-       }, 2000);
-      
+        document.title = "Textformatter - Home ";
+      }, 2000);
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
-      handleAlert('Light mode is enabledd', 'Success')
+      handleAlert("Light mode is enabledd", "Success");
     }
   };
   return (
     <>
-
-    
-    <Navbar
+   
+      <Navbar
         title="TextUtils"
         Home="Home"
         text={text}
@@ -56,11 +56,16 @@ function App() {
         Search="Search words"
         Searchbutton="search"
       />
-      <Alert alert={alert} />
+      <Alert alert={alert} mode={mode} />
       <div className="container">
-        <Form example="Say Something" setText={setText} handleAlert={handleAlert}/>
+        <Form
+          example="Say Something"
+          setText={setText}
+          handleAlert={handleAlert}
+        />
       </div>
       <AboutUs />
+      
     </>
   );
 }
